e297a4493d19eb35093a86b71846b5f239da65bc
Suat Ozcan <41187131+SuatOzcan@users.noreply.github.com>
2022-07-04 21:57:52 +0300

    Write first compute shader.

--- UnityComputeShaders - start/Assets/Scripts/AssignTexture.cs
+++ UnityComputeShaders - start/Assets/Scripts/AssignTexture.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class AssignTexture : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public ComputeShader shader;
+    public int texResolution = 256;
+
+    Renderer rend;
+    RenderTexture outputTexture;
+    int kernelHandle;
+
+    private void Start()
     {
-        
+        outputTexture = new RenderTexture(texResolution, texResolution, 0);
+        outputTexture.enableRandomWrite = true;
+        outputTexture.Create();
+
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+
+        InitShader();
+
+    }
+    private void InitShader()
+    {
+        kernelHandle = shader.FindKernel("CSMain");
+        shader.SetTexture(kernelHandle, "Result", outputTexture);
+        rend.material.SetTexture("_MainTex", outputTexture);
+        DispatchShader(texResolution / 16, texResolution / 16);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void DispatchShader(int x, int y)
     {
-        
+        shader.Dispatch(kernelHandle, x, y, 1);
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.U))
+        {
+            DispatchShader(texResolution / 8, texResolution / 8);
+        }
     }
 }
